8e0e45e716c5ef2f34cc18ac6eaef52261b92c8b
Parvizsho Aminov <p.aminov@yahoo.com>
2019-12-15 00:49:02 -0800

    adding missing import

--- features/base_feature.py
+++ features/base_feature.py
@@ -2,6 +2,7 @@ from google.colab import auth
 from oauth2client.client import GoogleCredentials
 import os
 import pandas as pd
+import pickle
 from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
 from sklearn.naive_bayes import MultinomialNB
